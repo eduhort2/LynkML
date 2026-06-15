0c9afef7632c89e60f64f929cb6dfa798739d07c
Hort <100052102+eduhort2@users.noreply.github.com>
2026-06-15 11:00:35 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.LYNK_CONFIG = {
-  supabaseUrl: "https://SEU-PROJETO.supabase.co",
+  supabaseUrl: "https://gaiufuhigbhfwaexrzsx.supabase.co/rest/v1/",
   supabaseAnonKey: "sb_publishable_564gsHfKsgAIZKKmjz17bQ_mbNfoFlG"
